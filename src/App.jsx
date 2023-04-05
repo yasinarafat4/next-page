fc2e3509100eada155c20438f13e8e936eb50f0d
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-05 15:17:08 +0600

    nav section responsive done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
-      {/* Header */}
+      <Header />
       <Outlet />
       {/* Footer */}
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  BoltIcon,
+  Bars3BottomRightIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="bg-gray-100 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div className="relative flex items-center justify-between ">
+        {/* Logo section */}
+        <Link to="/" className="inline-flex items-center">
+          <BoltIcon className="h-6 w-6 text-blue-500" />
+          <span className="ml-2 text-xl font-bold text-gray-800  tracking-wide">
+            nextPage
+          </span>
+        </Link>
+
+        {/* Nav item section */}
+
+        <ul className="items-center hidden space-x-8 lg:flex">
+          <li>
+            <NavLink
+              to="/"
+              title="Home Page"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "default"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/books"
+              title="Books"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "default"
+              }
+            >
+              Books
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              title="About Us Page"
+              className={({ isActive }) =>
+                isActive ? "text-blue-600" : "default"
+              }
+            >
+              About Us
+            </NavLink>
+          </li>
+        </ul>
+        {/* Mobile Navbar Section */}
+        <div className="lg:hidden">
+          {/* Dropdown Open Button */}
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className="w-5 text-gray-600" />
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 w-full z-10">
+              <div className="p-5 bg-white border rounded shadow-sm">
+                {/* Logo & Button section */}
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <Link to="/" className="inline-flex items-center">
+                      <BoltIcon className="h-6 w-6 text-blue-500" />
+                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                        nextPage
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className="w-5 text-gray-600" />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className="space-y-4">
+                    <li>
+                      <Link to="/" className="default">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/books"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
+                      >
+                        Books
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/about"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
+                      >
+                        About Us
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
